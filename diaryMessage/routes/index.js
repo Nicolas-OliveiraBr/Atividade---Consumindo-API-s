c55cfd4081fa28efcac9d9148a678826c5fca4f7
Nicolas Oliveira <dragonnightsky5@gmail.com>
2026-06-05 02:18:06 -0300

    Estilização concluída, adição de API de tradução das frases para o português

--- diaryMessage/routes/index.js
+++ diaryMessage/routes/index.js
@@ -2,16 +2,24 @@ var express = require('express');
 var router = express.Router();
 const axios = require('axios');
 
+async function translator(msg) {
+    const apiTranslator = await axios.get(`https://api.mymemory.translated.net/get?q=${msg}&langpair=en|pt-BR`); // Adicionei essa API de tradução pra português
+    // console.log(apiTranslator);
+    const translation = apiTranslator.data.responseData.translatedText
+    // console.log(translation);
+    return translation
+}
+
 /* GET home page. */
 router.get('/', async function(req, res, next) {
   try {
-    const response = await axios.get('https://ron-swanson-quotes.herokuapp.com/v2/quotes');
-    const message = response.data;
-    console.log(message);
-
+    const apiMessage = await axios.get('https://ron-swanson-quotes.herokuapp.com/v2/quotes');
+    const originalMessage = apiMessage.data;
+    const translatedMessage = await translator(originalMessage);
     res.render('index', { 
       title: 'Mensagem do dia:',
-      message
+      translatedMessage,
+      originalMessage
     });
   } catch(e) {
     console.log('Erro ao buscar mensagem.');
